877c4f7ca17ab8c7c7e8eda629378e020a2d7352
unknown <muszro@hotmail.com>
2017-05-10 15:35:38 +0200

    updated

---
+++ src/main/java/TextMessageSender/Main.java
@@ -0,0 +1,21 @@
+package TextMessageSender;
+
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import TextMessageSender.Producer;
+
+
+public class Main {
+
+	private static final Logger log = LoggerFactory.getLogger(Main.class);
+	
+	public static void main(String[] args) {
+			Producer sender = new Producer();
+			sender.sendTextMessages();;
+			sender.closeConnection();
+			System.exit(0);
+	}
+}
